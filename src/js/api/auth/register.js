859c144a392585dadd2125c09603a4a038e70007
H-chai <143179912+H-chai@users.noreply.github.com>
2024-09-05 09:10:36 +0200

    add Content-type and register.js (API)

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -1,8 +1,23 @@
-export async function register({
-  name,
-  email,
-  password,
-  bio,
-  banner,
-  avatar,
-}) {}
+import { headers } from "../headers"
+
+export async function register({url, data}) {
+  try {
+    const postData = {
+      method: "POST",
+      headers: headers(),
+      body: JSON.stringify(data),
+    };
+
+    const response = await fetch(url, postData);
+
+    if(!response.ok) {
+      throw new Error(`Server status: ${response.status}`);
+    }
+
+    const json = await response.json();
+
+    return json;
+  } catch(error) {
+    console.error(`Error message: ${error.message}`)
+  }
+}
--- src/js/api/headers.js
+++ src/js/api/headers.js
@@ -2,6 +2,7 @@ import { API_KEY } from "./constants";
 
 export function headers() {
   const headers = new Headers();
+  headers.append('Content-Type', 'application/json');
 
   if (API_KEY) {
     headers.append("X-Noroff-API-Key", API_KEY);
