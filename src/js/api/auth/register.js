b05ffa812df1743319944f75faeeef5a2f929c38
H-chai <143179912+H-chai@users.noreply.github.com>
2024-09-06 12:00:45 +0200

    Update register function with destructuring assignment

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -1,23 +1,41 @@
+import { API_AUTH_REGISTER } from "../constants";
 import { headers } from "../headers"
 
-export async function register(url, data) {
-  try {
-    const postData = {
-      method: "POST",
-      headers: headers(),
-      body: JSON.stringify(data),
-    };
+export async function register({ name, email, password }) {
+  const body = JSON.stringify({ name, email, password });
 
-    const response = await fetch(url, postData);
-    const json = await response.json();
-    console.log(json); //delete later!!
+  const response = await fetch(API_AUTH_REGISTER, {
+    headers: headers(),
+    method: "POST",
+    body,
+  });
 
-    if(!response.ok) {
-      throw new Error(`Error: ${json.errors[0].message}`);
-    }
-
-    return json;
-  } catch(error) {
-    throw error;
+  if(response.ok) {
+    const { data } = await response.json();
+    return data
   }
+
+  const errorData = await response.json();
+  const errorMessage = errorData.errors[0]?.message || "Could not register this account";
+  throw new Error(errorMessage);
+
+  // try {
+  //   const postData = {
+  //     method: "POST",
+  //     headers: headers(),
+  //     body: JSON.stringify(data),
+  //   };
+
+  //   const response = await fetch(url, postData);
+  //   const json = await response.json();
+  //   console.log(json); //delete later!!
+
+  //   if(!response.ok) {
+  //     throw new Error(`Error: ${json.errors[0].message}`);
+  //   }
+
+  //   return json;
+  // } catch(error) {
+  //   throw error;
+  // }
 }
--- src/js/ui/auth/register.js
+++ src/js/ui/auth/register.js
@@ -1,30 +1,23 @@
 import { register } from "../../api/auth/register";
-import { API_AUTH_REGISTER } from "../../api/constants";
 
 export async function onRegister(event) {
   event.preventDefault();
 
-  const usernameInput = document.getElementById("name");
+  const nameInput = document.getElementById("name");
   const emailInput = document.getElementById("email");
   const passwordInput = document.getElementById("password");
 
-  let username = usernameInput.value;
-  let email = emailInput.value;
-  let password = passwordInput.value;
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
 
-  const user = {
-    name : username,
-    email : email,
-    password : password
-  }
-
-  usernameInput.value = "";
+  nameInput.value = "";
   emailInput.value = "";
   passwordInput.value = "";
 
   try {
-    await register(API_AUTH_REGISTER, user);
-    alert(`Registration successful!\nUsername: ${user.name}\nEmail: ${user.email}`);
+    await register({ name, email, password });
+    alert(`Registration successful!\nUsername: ${name}\nEmail: ${email}`);
     window.location.href = "/auth/login/";
   } catch(error) {
     alert(`${error.message}.\nPlease try again.`);
