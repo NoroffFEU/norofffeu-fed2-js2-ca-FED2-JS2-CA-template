123c8e373d4853ca48315bda73b301297f0cb6a8
H-chai <143179912+H-chai@users.noreply.github.com>
2024-09-09 09:37:28 +0200

    refactor the response handling

--- src/js/api/headers.js
+++ src/js/api/headers.js
@@ -1,11 +1,18 @@
 import { API_KEY } from "./constants";
 
-export function headers() {
+export function headers(body) {
   const headers = new Headers();
-  headers.append('Content-Type', 'application/json');
 
   if (API_KEY) {
     headers.append("X-Noroff-API-Key", API_KEY);
+  }
+
+  if(localStorage.token) {
+    headers.append("Authorization", `Bearer ${localStorage.token}`)
+  }
+
+  if(body) {
+    headers.append('Content-Type', 'application/json');
   }
 
   return headers;
--- src/js/api/index.js
+++ src/js/api/index.js
@@ -17,42 +17,79 @@ export default class NoroffAPI {
       const body = JSON.stringify({ email, password });
   
       const response = await fetch(this.apiLoginPath, {
-        headers: headers(),
+        headers: headers(true),
         method: "POST",
         body,
       });
+
+      const data = await this.util.handleResponse(response, "Could not login this account");
+      const { accessToken: token, ...user } = data;
+      localStorage.token = token;
+      localStorage.user = JSON.stringify(user);
+      window.location.href = "/post/feed/";
+      return data;
     
-      if(response.ok) {
-        const { data } = await response.json();
-        const { accessToken: token, ...user } = data;
-        localStorage.token = token;
-        localStorage.user = JSON.stringify(user);
-        window.location.href = "/post/feed/";
-        return data;
-      }
+      // if(response.ok) {
+      //   const { data } = await response.json();
+      //   const { accessToken: token, ...user } = data;
+      //   localStorage.token = token;
+      //   localStorage.user = JSON.stringify(user);
+      //   window.location.href = "/post/feed/";
+      //   return data;
+      // }
       
-      const errorData = await response.json();
-      const errorMessage = errorData.errors[0]?.message || "Could not login with this account";
-      throw new Error(errorMessage);
+      // const errorData = await response.json();
+      // const errorMessage = errorData.errors[0]?.message || "Could not login with this account";
+      // throw new Error(errorMessage);
     },
 
     register: async ({ name, email, password }) => {
       const body = JSON.stringify({ name, email, password });
   
       const response = await fetch(this.apiRegisterPath, {
-        headers: headers(),
+        headers: headers(true),
         method: "POST",
         body,
       });
+
+      const data = await this.util.handleResponse(response, "Could not register this account");
+      return data;
   
-      if(response.ok) {
-        const { data } = await response.json();
-        return data
-      }
+      // if(response.ok) {
+      //   const { data } = await response.json();
+      //   return data
+      // }
   
-      const errorData = await response.json();
-      const errorMessage = errorData.errors[0]?.message || "Could not register this account";
-      throw new Error(errorMessage);
+      // const errorData = await response.json();
+      // const errorMessage = errorData.errors[0]?.message || "Could not register this account";
+      // throw new Error(errorMessage);
     },
+  }
+
+  util = {
+    handleResponse: async (response, errorMessage) => {
+      if (response.ok) {
+        return await response.json();
+      }
+
+      const errorData = await response.json();
+      const errorDetail = errorData.errors[0]?.message || "Unknown error";
+
+      throw new Error(`${errorMessage}: ${errorDetail}`);
+    }
+  }
+
+  post = {
+    create: async ({ title, body, tags, media }) => {},
+
+    delete: async (id) => {},
+
+    readPost: async (id) => {},
+
+    readPosts: async () => {},
+
+    readPostsByUser: async () => {},
+
+    update: async (id, { title, body, tags, media }) => {},
   }
 }
