3e765105b06f4d53cdbb94a3185079b64dc0def4
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-09-18 13:07:59 +0200

    cleaning code a bit

--- src/components/cards/PostCard.ts
+++ src/components/cards/PostCard.ts
@@ -1,45 +1,106 @@
-// import { PostResponse } from "@/types/types";
-// import { DeleteButton } from "@/components/buttons/DeleteButton";
+import { PostResponse } from "@/types/types";
+import { FollowButton } from "@/components/buttons/FollowButton";
+import { LikeButton } from "@/components/buttons/LikeButton";
+import { DeleteButton } from "@/components/buttons/DeleteButton";
 
-// const postCardTemplate = document.createElement("template");
+export function createPostHTML(
+  post: PostResponse,
+  isFollowing: boolean,
+  isLiked: boolean,
+  isUserPost: boolean
+) {
+  if (!customElements.get("follow-button")) {
+    customElements.define("follow-button", FollowButton);
+  }
 
-// postCardTemplate.innerHTML = `
-// <li>
-//   <div>
-//     <h3 id="username"></h3>
-//     <delete-button></delete-button>
-//   </div>
-// </li>
-// `;
+  if (!customElements.get("like-button")) {
+    customElements.define("like-button", LikeButton);
+  }
 
-// export class PostCard extends HTMLElement {
-//   private _post: PostResponse | null = null;
+  if (!customElements.get("delete-button")) {
+    customElements.define("delete-button", DeleteButton);
+  }
+  return `
+  <li class="post" data-post-id="${post.id}">
+    <div class="post__header">
+        <div>
+            <div class="post__header__avatar">
+                <img 
+                    src="${post.author.avatar.url}" 
+                    alt="${post.author.name} avatar" 
+                />
+            </div>
 
-//   constructor() {
-//     super();
-//     this.attachShadow({ mode: "open" });
-//     if (this.shadowRoot) {
-//       this.shadowRoot.appendChild(postCardTemplate.content.cloneNode(true));
-//     }
-//   }
+            <div class="post__header__user">
+                <span>${post.author.name}</span>
+                <a href="/profile/?username=${post.author.name}">
+                    @${post.author.name}
+                </a>
+            </div>
 
-//   set post(data: PostResponse) {
-//     this._post = data;
-//     this.render();
-//   }
+            </div>
+            ${
+              isUserPost
+                ? `
+                <div class="user-buttons">
+                    <delete-button 
+                        data-post-id="${post.id}" 
+                        data-is-user-post="${isUserPost}"
+                    >
+                        Delete
+                    </delete-button>
+                    <a 
+                        class="edit-btn" 
+                        href="/post/edit/?id=${post.id}"
+                    >
+                        Edit
+                    </a>
+                </div>`
+                : `
+                <div>
+                    <follow-button 
+                        data-user-name="${post.author.name}" 
+                        data-is-following="${isFollowing}" 
+                        data-is-user-post="${isUserPost}"
+                    ></follow-button>
+                </div>
+            `
+            }
+        </div>
 
-//   render() {
-//     console.log(this._post);
-//     if (this.shadowRoot && this._post) {
-//       const username = this.shadowRoot.querySelector("#username");
+    <div class="post__body">
+        <a href="/post/?id=${post.id}">
+            ${post.title}
+        </a>
+        <p>${post.body}</p>
 
-//       if (username) {
-//         username.textContent = this._post.author.name;
-//       }
-//     }
-//   }
-
-//   renderDeleteButton() {}
-// }
-
-// customElements.define("post-card", PostCard);
+            ${
+              post.media?.url
+                ? `<img class="post__body__media" src="${post.media.url}" alt="${post.title} media" />`
+                : ""
+            }
+            ${
+              post.tags.length > 0
+                ? `<div class="post__body__tags">${post.tags
+                    .map((tag) => `<span>#${tag}</span>`)
+                    .join("")}</div>`
+                : ""
+            }
+    </div>
+        
+    <div class="post__footer">
+        <div>
+            <a href="/post/?id=${post.id}">View</a>
+            <a href="/post/?id=${post.id}">Reply</a>
+        </div>
+        <like-button 
+            data-post-id="${post.id}" 
+            data-reactions="${post._count.reactions}" 
+            data-user-liked="${isLiked}"
+        >
+        </like-button>
+    </div>
+        <hr>
+  </li>
+    `;
+}
--- src/js/router/views/explore.ts
+++ src/js/router/views/explore.ts
@@ -2,10 +2,8 @@ import { readPosts } from "@/js/api/post/read";
 import { AllPostsResponse, Meta } from "@/types/types";
 import { getUser } from "@/js/utilities/getUser";
 import { getUserProfile } from "@/js/utilities/getUserProfile";
-import { FollowButton } from "@/components/buttons/FollowButton";
-import { LikeButton } from "@/components/buttons/LikeButton";
-import { DeleteButton } from "@/components/buttons/DeleteButton";
 import { authGuard } from "@/js/utilities/authGuard";
+import { createPostHTML } from "@/components/cards/PostCard";
 
 async function loadExplorePage() {
   try {
@@ -18,18 +16,6 @@ async function loadExplorePage() {
 export async function renderAllPosts(page: number = 1) {
   const postsContainer = document.getElementById("posts") as HTMLUListElement;
 
-  if (!customElements.get("follow-button")) {
-    customElements.define("follow-button", FollowButton);
-  }
-
-  if (!customElements.get("like-button")) {
-    customElements.define("like-button", LikeButton);
-  }
-
-  if (!customElements.get("delete-button")) {
-    customElements.define("delete-button", DeleteButton);
-  }
-
   const getAllPosts = (await readPosts({ page: page })) as AllPostsResponse;
 
   const { data, meta } = getAllPosts;
@@ -40,10 +26,6 @@ export async function renderAllPosts(page: number = 1) {
     postsContainer.innerHTML = "No posts found";
   } else {
     data.forEach(async (post) => {
-      const li = document.createElement("li");
-      li.setAttribute("data-post-id", post.id.toString());
-      li.classList.add("post");
-
       const isFollowing = getFollowingUsers?.find(
         (user) => user.name === post.author.name
       )
@@ -58,93 +40,8 @@ export async function renderAllPosts(page: number = 1) {
 
       const isUserPost = post.author.name === getUser() ? true : false;
 
-      console.log(post.tags);
-
-      li.innerHTML = `
-              <div class="post__header">
-                <div>
-                  <div class="post__header__avatar">
-                    <img 
-                      src="${post.author.avatar.url}" 
-                      alt="${post.author.name} avatar" 
-                    />
-                  </div>
-                  <div class="post__header__user">
-                    <span>${post.author.name}</span>
-                    <a 
-                      href="/profile/?username=${post.author.name}"
-                    >
-                      @${post.author.name}
-                    </a>
-                  </div>
-                </div>
-
-                ${
-                  isUserPost
-                    ? `
-                    <div class="user-buttons">
-                      <delete-button 
-                        data-post-id="${post.id}" 
-                        data-is-user-post="${isUserPost}"
-                      >
-                        Delete
-                      </delete-button>
-                      <a class="edit-btn" href="/post/edit/?id=${post.id}">
-                        Edit
-                      </a>
-                    </div>
-                  `
-                    : `
-                    <div>
-                      <follow-button 
-                        data-user-name="${post.author.name}" 
-                        data-is-following="${isFollowing}" 
-                        data-is-user-post="${isUserPost}"
-                      >
-                      </follow-button>
-                    </div>
-                    `
-                }
-
-              </div>
-
-              <div class="post__body">
-                <a href="/post/?id=${post.id}">${post.title}</a>
-                <p>${post.body}</p>
-                <div>
-                  ${
-                    post.media?.url
-                      ? `<img class="post__body__media" src="${post.media.url}" alt="${post.title} media" />`
-                      : ""
-                  }
-                </div>
-                ${
-                  post.tags.length > 0 && post.tags[0] !== ""
-                    ? `<div class="post__body__tags">${post.tags
-                        .map((tag) => `<span>#${tag}</span>`)
-                        .join("")}</div>`
-                    : ""
-                }
-              </div>
-
-
-              <div class="post__footer">
-                <div>
-                  <a href="/post/?id=${post.id}">View</a>
-                  <a href="/post/?id=${post.id}">Reply</a>
-                </div>
-      
-                <like-button
-                  data-post-id="${post.id}"
-                  data-reactions="${post._count.reactions}"
-                  data-user-liked="${isLiked}"
-                  >
-                </like-button>
-              </div>
-              <hr>
-         `;
-
-      postsContainer.appendChild(li);
+      const postHTML = createPostHTML(post, isFollowing, isLiked, isUserPost);
+      postsContainer.insertAdjacentHTML("beforeend", postHTML);
     });
   }
 }
