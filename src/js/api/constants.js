503914e1da1fa518eaa8bc489e203d2749524cb4
RosarioBA <146356539+RosarioBA@users.noreply.github.com>
2024-09-19 13:15:59 +0200

    added API_KEY

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -1,6 +1,6 @@
 // Use Postman, or JavaScript to get your API key
 // In Workflow we will learn how to secure this information
-export const API_KEY = "replace-with-your-key";
+export const API_KEY = 'ba9d30d1-0826-485a-8392-e121c9410188';
 
 export const API_BASE = "https://v2.api.noroff.dev";
 
