5ccc5b5f1a534f46a2a0b904c7006d5981d9234d
LAPTOP-4RIM9K97\Panda <chrisrberg03@gmail.com>
2024-09-22 21:43:29 +0200

    Change: added id to post containers. added a logout function that can be called where ever

--- src/js/ui/global/logout.js
+++ src/js/ui/global/logout.js
@@ -1,1 +1,9 @@
-export function setLogoutListener() {}
+export function setLogoutListener() {
+    const logoutBtn = document.querySelector(".logoutBtn");
+
+    logoutBtn.addEventListener("click", function () {
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("userID");
+        window.location.href = "/auth/login/";
+    });
+}
