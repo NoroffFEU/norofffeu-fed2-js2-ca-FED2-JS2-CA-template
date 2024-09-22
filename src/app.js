b076cbb6e97d9183c397e8d629be93784c366ecd
RosarioBA <146356539+RosarioBA@users.noreply.github.com>
2024-09-22 19:54:40 +0200

    Update app initialization and token checking

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 // src/app.js
-
 import router from "./js/router/index.js";
 
 document.addEventListener('DOMContentLoaded', () => {
+  console.log('App loaded');
+  console.log('Token:', localStorage.getItem('token'));
   router(window.location.pathname);
 });
