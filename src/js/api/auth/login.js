8e80c24191fad0af7ea34f9d67eedaaf4c43bbf6
LAPTOP-4RIM9K97\Panda <chrisrberg03@gmail.com>
2024-09-22 15:25:31 +0200

    feat: added login with auth. Does not yet work optimal, need to fix localstorage as undefined and submit btn not working

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,1 +1,25 @@
-export async function login({ email, password }) {}
+import { API_AUTH_LOGIN, API_KEY } from "../constants.js";
+
+export async function login({ email, password }) {
+    try {
+        const response = await fetch(API_AUTH_LOGIN, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-Noroff-API-Key": API_KEY,
+            },
+            body: JSON.stringify({ email, password }),
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            return { ok: true, data };
+        }
+
+        throw new Error("Failed to login: email or password is incorrect");
+    } catch (error) {
+        console.error(error);
+        return { ok: false, error: error.message };
+    }
+}
