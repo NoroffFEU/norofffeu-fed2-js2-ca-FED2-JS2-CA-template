67783605f2768db508761a08f7a69829643ce80e
H-chai <143179912+H-chai@users.noreply.github.com>
2024-09-24 17:15:03 +0200

    logo path

--- src/app.js
+++ src/app.js
@@ -1,35 +1,7 @@
 import "./css/style.css";
-//import { API_KEY } from "./js/api/constants.js";
 import NoroffApp from "./js/ui/index.js";
 
 const app = new NoroffApp();
 
 import NoroffAPI from "./js/api/index.js";
 const api = new NoroffAPI();
-
-// async function allProfiles() {
-//   try {
-//     const response = await fetch("https://v2.api.noroff.dev/social/profiles", {
-//       method: "GET",
-//       headers: {
-//         "Content-Type": "application/json",
-//         "X-Noroff-API-Key": API_KEY,
-//         "Authorization": `Bearer ${localStorage.token}`,
-//       }
-//     });
-
-//     if (!response.ok) {
-//       throw new Error(`Error: ${response.status} ${response.statusText}`);
-//     }
-
-//     const json = await response.json();
-//     console.log(json);
-
-//     return json;
-//   } catch (error) {
-//     console.error("Failed to fetch profiles:", error);
-//     throw error;  // Optionally, re-throw the error to handle it elsewhere
-//   }
-// }
-
-// allProfiles();
