9ef5463d6cee8ca0a0d0d80aaba2ebd6a24958b2
RosarioBA <146356539+RosarioBA@users.noreply.github.com>
2024-09-24 18:58:30 +0200

    enhance readPosts function to include author data

--- src/js/api/post/read.js
+++ src/js/api/post/read.js
@@ -2,19 +2,21 @@ import { API_BASE } from "../constants.js";
 import { headers } from "../headers.js";
 
 export async function readPosts(limit = 12, page = 1, tag) {
-  const queryParams = new URLSearchParams({ limit, page });
-  if (tag) queryParams.append("_tag", tag);
-  console.log('Headers:', Object.fromEntries(headers().entries()));
-  const response = await fetch(`${API_BASE}/social/posts?${queryParams}`, {
-    headers: headers(),
-  });
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+    const queryParams = new URLSearchParams({ limit, page, _author: 'true' });
+    if (tag) queryParams.append("_tag", tag);
+  
+    console.log('Headers:', Object.fromEntries(headers().entries()));
+  
+    const response = await fetch(`${API_BASE}/social/posts?${queryParams}`, {
+      headers: headers(),
+    });
+  
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+  
+    return await response.json();
   }
-
-  return await response.json();
-}
 
 export async function readPost(id) {
   const response = await fetch(`${API_BASE}/social/posts/${id}`, {
