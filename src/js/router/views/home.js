e686e11356530e56d239d4e49f53ae2d13330594
RosarioBA <146356539+RosarioBA@users.noreply.github.com>
2024-09-24 18:40:32 +0200

    update home view to display posts

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,7 +1,21 @@
 import { authGuard } from "../../utilities/authGuard.js";
+import { displayPosts } from "../../ui/post/list.js";
 
 export default function homeView() {
   console.log('Home view loaded');
+  
+  // Run the authentication guard
   authGuard();
-  // Add your home page logic here
+
+  // Update the main content
+  const main = document.querySelector("main");
+  main.innerHTML = `
+    <h1>Home</h1>
+    <div id="posts-container"></div>
+  `;
+  
+  // Display the posts
+  displayPosts();
+
+  // You can add any additional home page logic here
 }
