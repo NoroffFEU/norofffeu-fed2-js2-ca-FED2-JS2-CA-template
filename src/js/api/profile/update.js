3e9ba6d794f720755a99ef43c05c60810ac63698
Erica Sheidai <erishe01272@stud.noroff.no>
2024-09-26 14:51:33 +0200

    feat: profile page, unfinished

--- src/js/api/profile/update.js
+++ src/js/api/profile/update.js
@@ -1,1 +1,18 @@
-export async function updateProfile(username, { avatar, banner }) {}
+import { API_KEY, API_SOCIAL_PROFILES } from "../constants.js";
+
+export async function updateProfile(userID, { avatar, banner, bio }) {
+    const response = await fetch(`${API_SOCIAL_PROFILES}/${userID}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+            "X-Noroff-API-Key": API_KEY,
+        },
+        body: JSON.stringify({ avatar: { url: avatar, alt: "User Avatar" }, banner: { url: banner, alt: "User Banner" }, bio }),
+    });
+
+    if (!response.ok) {
+        throw new Error("Could not update profile.");
+    }
+    return await response.json();
+}
