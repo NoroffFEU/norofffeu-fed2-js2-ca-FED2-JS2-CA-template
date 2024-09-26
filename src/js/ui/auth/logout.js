01d38fe5463ede993d72e8753c2b12bed61e67a1
RosarioBA <146356539+RosarioBA@users.noreply.github.com>
2024-09-26 18:06:40 +0200

    creating logout function

--- src/js/ui/auth/logout.js
+++ src/js/ui/auth/logout.js
@@ -1,1 +1,10 @@
-export function onLogout() {}
+import { updateNavigation } from '../../../app.js';
+
+export function onLogout() {
+  localStorage.removeItem('token');
+  localStorage.removeItem('user');
+  updateNavigation(); // Call this after removing the token
+  window.location.href = '/auth/login/'; // Redirect to login page
+}
+
+export default onLogout;
