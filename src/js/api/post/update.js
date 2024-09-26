b90a1c0ce3a9bcf116c3bbd4efa13e3e5eefa3fe
ukonuidika <120014847+ukonuidika@users.noreply.github.com>
2024-09-26 22:09:57 +0200

    Update files
    src\js\api\post\comment.js
    src\js\api\post\create.js
    src\js\api\post\delete.js
    src\js\api\post\react.js
    src\js\api\post\read.js
    src\js\api\post\reply.js
    src\js\api\post\update.js

--- src/js/api/post/update.js
+++ src/js/api/post/update.js
@@ -1,1 +1,36 @@
-export async function updatePost(id, { title, body, tags, media }) {}
+import { getToken } from "../../utilities/token";
+import { API_KEY, API_SOCIAL_POSTS } from "../constants";
+import { headers } from "../headers";
+
+export async function updatePost(id, { title, body, tags, media }) {
+  try {
+    const token = getToken();
+    const postData = {
+      title,
+      body,
+      tags,
+      media: { url: media.url },
+    };
+
+    const response = await fetch(`${API_SOCIAL_POSTS}/${id}`, {
+      method: "PUT",
+      headers: {
+        ...headers(),
+        Authorization: `Bearer ${token}`,
+        "X-Noroff-API-Key": API_KEY,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postData),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to update the post");
+    }
+
+    const updatedPost = await response.json();
+    return updatedPost;
+  } catch (error) {
+    console.error("Error updating post:", error);
+    throw error;
+  }
+}
