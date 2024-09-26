c14fb7b319c435eb29511f7c995a4beacb503dc5
ukonuidika <120014847+ukonuidika@users.noreply.github.com>
2024-09-27 01:30:53 +0200

    Update file
    src\js\ui\global\logout.js

--- src/js/ui/global/logout.js
+++ src/js/ui/global/logout.js
@@ -1,1 +1,12 @@
-export function setLogoutListener() {}
+import { onLogout } from "../auth/logout";
+
+export function setLogoutListener() {
+  const logoutButtons = document.querySelectorAll("#logout");
+
+  logoutButtons.forEach((button) => {
+    button.addEventListener("click", function (event) {
+      event.preventDefault();
+      onLogout();
+    });
+  });
+}
