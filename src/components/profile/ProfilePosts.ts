ceec5ab24061e5b19774f3705c80cf63a3ffcf26
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-09-27 10:11:49 +0200

    delete more code not used

--- src/components/profile/ProfilePosts.ts
+++
@@ -1,5 +0,0 @@
-import { PostResponse } from "@/types/types";
-
-export function createProfilePostsHTML(posts: PostResponse[]) {
-  console.log(posts);
-}
--- src/components/profile/ProfileRenderer.ts
+++
@@ -1,1 +0,0 @@
-export async function renderProfile(username: string) {}
