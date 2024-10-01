07b525217c1ec0e6065f0f6f3889bc19bf2f6fe6
Erica Sheidai <erishe01272@stud.noroff.no>
2024-10-01 19:48:12 +0200

    Chore: cleanup + readme

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,5 +1,15 @@
 import { API_AUTH_LOGIN, API_KEY } from "../constants.js";
 
+/**
+ * Logs in a user with the provided email and password.
+ *
+ * @param {Object} credentials - The login credentials.
+ * @param {string} credentials.email - The user's email address.
+ * @param {string} credentials.password - The user's password.
+ * @returns {Promise<Object>} - A promise that resolves to an object containing the login result.
+ *                              - If successful, the object has the structure: { ok: true, data: <user data> }
+ *                              - If unsuccessful, the object has the structure: { ok: false, error: <error message> }
+ */
 export async function login({ email, password }) {
     try {
         const response = await fetch(API_AUTH_LOGIN, {
--- src/js/router/views/login.js
+++ src/js/router/views/login.js
@@ -2,6 +2,11 @@ import { onLogin } from "../../ui/auth/login.js";
 
 const form = document.forms.login;
 
+/**
+ * Initializes the login form by adding a submit event listener.
+ * The listener calls the `onLogin` function when the form is submitted.
+ * If the form is not found, an error message is logged to the console.
+ */
 if (form) {
     form.addEventListener("submit", onLogin);
 } else {
