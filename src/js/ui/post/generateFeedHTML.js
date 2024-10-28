2582222feb1339118c18459952f364927e812555
H-chai <143179912+H-chai@users.noreply.github.com>
2024-10-28 10:13:07 +0100

    Apply css (responsive)

--- src/js/ui/post/generateFeedHTML.js
+++ src/js/ui/post/generateFeedHTML.js
@@ -1,15 +1,23 @@
 import { formatDate } from "../../utilities/formatDate";
 
 export function generateFeedHTML(post) {
-  const postContainer = document.createElement('a');
-  postContainer.classList.add('post-container');
+  const postContainer = document.createElement("a");
+  postContainer.classList.add(
+    "post-container",
+    "d-block",
+    "overflow-hidden",
+    "border",
+    "rounded",
+    "p-2",
+  );
   postContainer.id = post.id;
   postContainer.href = `/post/?id=${post.id}`;
 
   const figure = document.createElement("figure");
+  figure.classList.add("ratio", "ratio-4x3", "m-0");
   const thumbnail = document.createElement("img");
-  thumbnail.classList.add("thumbnail");
-  if(post.media?.url) {
+  thumbnail.classList.add("thumbnail", "rounded", "object-fit-fill");
+  if (post.media?.url) {
     thumbnail.src = post.media.url;
     thumbnail.alt = post.media.alt;
 
@@ -22,55 +30,99 @@ export function generateFeedHTML(post) {
     thumbnail.alt = "No Media Available";
   }
   figure.appendChild(thumbnail);
-  
-  const postTextContainer = document.createElement('div');
-  postTextContainer.classList.add('post-text-container');
 
-  const postUserDate = document.createElement('div');
-  postUserDate.classList.add("post-user-date");
+  const postTextContainer = document.createElement("div");
+  postTextContainer.classList.add(
+    "post-text-container",
+    "px-2",
+    "pt-2",
+    "pb-5",
+  );
+
+  const postUserDate = document.createElement("div");
+  postUserDate.classList.add(
+    "post-user-date",
+    "d-flex",
+    "justify-content-between",
+    "mb-3",
+  );
   const postUserContainer = document.createElement("div");
   postUserContainer.classList.add("user");
   const postUserIcon = document.createElement("i");
-  postUserIcon.classList.add("fa-regular", "fa-user", "user-icon");
+  postUserIcon.classList.add("fa-regular", "fa-user", "user-icon", "me-1");
   const userName = document.createElement("a");
   userName.classList.add("post-author");
   userName.textContent = post.author.name;
   userName.href = `/profile/?name=${post.author.name}`;
   postUserContainer.append(postUserIcon, userName);
 
-  const postDateContainer =  document.createElement("div");
-  postDateContainer.classList.add("date");
+  const postDateContainer = document.createElement("div");
+  postDateContainer.classList.add(
+    "date",
+    "d-flex",
+    "justify-content-between",
+    "align-items-center",
+  );
   const postDateIcon = document.createElement("i");
-  postDateIcon.classList.add("fa-regular", "fa-calendar");
+  postDateIcon.classList.add("fa-regular", "fa-calendar", "me-1");
   const postDate = document.createElement("p");
+  postDate.classList.add("mb-0");
   postDate.textContent = formatDate(post.created);
   postDateContainer.append(postDateIcon, postDate);
 
   postUserDate.append(postUserContainer, postDateContainer);
 
-  const postTagComment = document.createElement('div');
-  postTagComment.classList.add("post-tag-comment");
-  const tagList = document.createElement('ul');
-  tagList.classList.add('tag-list');
+  const postTagComment = document.createElement("div");
+  postTagComment.classList.add(
+    "post-tag-comment",
+    "d-flex",
+    "justify-content-between",
+    "align-items-center",
+    "mb-3",
+  );
+  const tagList = document.createElement("ul");
+  tagList.classList.add(
+    "tag-list",
+    "list-unstyled",
+    "d-flex",
+    "align-items-center",
+    "gap-1",
+    "mb-0",
+    "flex-wrap",
+  );
   const tagsArray = post.tags;
-  tagsArray?.filter(tag => tag.trim().length > 0).forEach(tag => {
-    const tagItem = document.createElement('li');
-    tagItem.classList.add('tag-item');
-    tagItem.textContent = tag;
-    tagList.appendChild(tagItem);
-  });
+  tagsArray
+    ?.filter((tag) => tag.trim().length > 0)
+    .forEach((tag) => {
+      const tagItem = document.createElement("li");
+      tagItem.classList.add(
+        "tag-item",
+        "border",
+        "px-2",
+        "py-1",
+        "rounded-pill",
+      );
+      tagItem.textContent = tag;
+      tagList.appendChild(tagItem);
+    });
   const comment = document.createElement("div");
-  comment.classList.add("comment");
+  comment.classList.add(
+    "comment",
+    "d-flex",
+    "justify-content-between",
+    "align-items-center",
+  );
   const commentIcon = document.createElement("i");
-  commentIcon.classList.add("fa-regular", "fa-comments");
+  commentIcon.classList.add("fa-regular", "fa-comments", "me-1");
   const commentNumber = document.createElement("p");
+  commentNumber.classList.add("mb-0");
   commentNumber.textContent = post.comments.length;
   comment.append(commentIcon, commentNumber);
 
   postTagComment.append(tagList, comment);
 
   const postTitle = document.createElement("p");
-  postTitle.classList.add("post-title");
+  postTitle.classList.add("post-title", "h3");
   postTitle.textContent = post.title;
 
   postTextContainer.append(postUserDate, postTagComment, postTitle);
