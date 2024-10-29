de08833b00ae86145ecf5e04a77eb28acd0db8a9
natnoppol <126169607+natnoppol@users.noreply.github.com>
2024-10-29 22:07:53 +0100

    navbar tailwind need to be fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js}"],
@@ -5,5 +6,4 @@ export default {
     extend: {},
   },
   plugins: [],
-}
-
+};
