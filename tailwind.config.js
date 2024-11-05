dbb28943acd6c5e81de140e819fe3e709c411f56
RosarioBA <146356539+RosarioBA@users.noreply.github.com>
2024-11-05 21:40:05 +0100

    feat: initialize Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./auth/**/*.{html,js}",
+    "./post/**/*.{html,js}",
+    "./profile/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
