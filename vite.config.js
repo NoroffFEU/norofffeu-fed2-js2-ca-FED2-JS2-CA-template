5cf3c438c509272a96982ec5b09a71ff1f29069b
shirwac <shirwacstudy@gmail.com>
2024-11-09 04:57:28 +0100

    errors

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/fed2-js2-ca-shiwa4656/' : '/', // Adjust this to match your GitHub Pages repository name
   appType: "mpa",
   build: {
     target: 'es2022',
