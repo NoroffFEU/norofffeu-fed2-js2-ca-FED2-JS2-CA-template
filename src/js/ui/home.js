be5bf7a425e61d31d2f7cb1b3461f417f3df8bda
sandygrandy <langeng.sandra@gmail.com>
2024-12-19 21:40:31 +0100

    styled: auth, home

--- src/js/ui/home.js
+++ src/js/ui/home.js
@@ -1,15 +1,15 @@
 // Showing posts on home page. This is the same as the posts page, but it shows all posts by all users.
 export async function onReadPosts(posts) {
-    const postsList = document.getElementById("posts-list");
-    posts.forEach((post) => {
-        const postTemplate = document.getElementById("post-template");
-        const postClone = postTemplate.content.cloneNode(true);
-        postClone.children[0].addEventListener("click", () => {
-            window.location.href = `/post/?id=${post.id}`;
-        });
-        postClone.getElementById("title").textContent = post.title;
-        postClone.getElementById("content").textContent = post.body;
-        postsList.appendChild(postClone);
+  const postsList = document.getElementById("posts-list");
+  posts.forEach((post) => {
+    const postTemplate = document.getElementById("post-template");
+    const postClone = postTemplate.content.cloneNode(true);
+    postClone.children[0].addEventListener("click", () => {
+      window.location.href = `/post/?id=${post.id}`;
     });
+    postClone.getElementById("title").textContent = post.title;
+    postClone.getElementById("content").textContent = post.body;
+    postClone.getElementById("card-img-top").src = post.media.url;
+    postsList.appendChild(postClone);
+  });
 }
-
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = {
+  content: ["./**/*.{html,js}", "!./node_modules/**/*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
